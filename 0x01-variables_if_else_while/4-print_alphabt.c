b89f56c45ed08c1d9334a9e28b9f46a7f409507c
esteem-boy <bawaabdulsilas@gmail.com>
2023-02-18 01:59:03 -0800

    hen I was having that alphabet soup, I never thought that it would pay off.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This is the main function of the C code
+ *
+ * Return: 0 Successful
+ *
+ */
+int main(void) /* main - Another main desription */
+{
+	int i;
+
+	i = 'a';
+	for (; i <= 'z' ;)
+	{
+		if (i == 'q' || i == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar('a' + i);
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
