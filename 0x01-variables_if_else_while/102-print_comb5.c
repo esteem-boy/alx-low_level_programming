5722bff96fc1af26fc7feb87a6a9cfdbfa56de64
esteem-boy <bawaabdulsilas@gmail.com>
2023-02-19 19:58:45 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Main function of the program
+ *
+ * Return: (0) Alway refurn success
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	/* more code goes here */
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (l = 1; l <= 9; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+					putchar('0' + l);
+					if ((i != 9) || (j != 9))
+					{
+						putchar(' ');
+					}
+					else if ((k != 9) || (l != 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
