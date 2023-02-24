3af6db0ebb691d882bb6fdbd75cf0ee1c3848f42
esteem-boy <bawaabdulsilas@gmail.com>
2023-02-23 17:32:55 -0800

    I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* print_diagonal - this func prints out a horizontal line on the terminal
+*
+* @n: it is the argument of the func
+*
+* Return: functions of this type do necessarily have return anything
+*
+*/
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
