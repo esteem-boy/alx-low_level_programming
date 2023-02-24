29e3a5c3fdf28d0fc161fcdf11db9211c5145deb
esteem-boy <bawaabdulsilas@gmail.com>
2023-02-23 18:51:42 -0800

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_numbers - this func prints out numbers ranging from 0 to 9
+* print_most_numbers - this func prints out numbers ranging from 0 to 9
 *
 * Return: this returns 1 if is the char checked is true and 0 if otherwise
 *
@@ -10,17 +10,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = 0;
-		while (i <= 9)
+		for (i = 0; i <= 9; i++)
 		{
-			if ((i != 2) && (i != 4))
+			if (!(i == 2 || i == 4))
 			{
-				continue;
-			}
-			else
-			{
-				_putchar('0' + i);
-				i++;
+				_putchar(i + '0');
 			}
 		}
 	_putchar('\n');
