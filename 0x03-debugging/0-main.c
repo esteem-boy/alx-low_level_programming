d0224ff861811fcbeb3ffa5b33a871820130d2bf
esteem-boy <bawaabdulsilas@gmail.com>
2023-02-24 16:15:48 -0800

    Like, comment, subscribe

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * positive_or_negative -as the name implies func return +ve or -ve num
+ *
+ * Return: not value for funtions of these type
+ *
+ */
+void positive_or_negative()
+/**
+ * {
+ * printf("%d", i);
+ * printf(" is zero\n");
+ *
+}*/
