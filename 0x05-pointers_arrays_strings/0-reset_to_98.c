2bb7abcc5c42ca7c8396707930f31aa0efeaddfb
esteem-boy <bawaabdulsilas@gmail.com>
2023-02-27 23:04:18 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - this updated the value of int var
+ *
+ * Return: this func usually wnt return any value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
