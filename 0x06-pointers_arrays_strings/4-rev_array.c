f767bc6a3c0bf581041070711ee7571a9e468764
esteem-boy <bawaabdulsilas@gmail.com>
2023-03-02 16:06:18 -0800

    Always look up

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,5 +12,13 @@
  */
 void reverse_array(int *a, int n)
 {
+	int i;
+	int newArr;
 
+	for  (i = 0; i < (n / 2); i++)
+	{
+		newArr = a[n];
+		a[n] = a[n - i - 1];
+		a[n - i - 1] = newArr;
+	}
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 #include "main.h"
 
 /**
- * *string_toupper - this function changes all lowercase letters of a string to uppercase.
+ * *string_toupper - this func converts all lowercases to uppercases
  *
- * @*: argument
+ * @str: argument
  *
  * Return: Always 0
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
+	int i;
 
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
 }
