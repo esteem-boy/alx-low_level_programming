9ce51a8a7380533a9afcef666f06047165f2ddd5
esteem-boy <bawaabdulsilas@gmail.com>
2023-03-02 18:20:25 -0800

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,51 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "main.h"
 
 /**
  * *cap_string - This changes to capitals letters all words of a string
  *
- * @*: argument
+ * @str: argument
  *
  * Return: is 0;
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		else if (i == ' ')
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 32;
+			}
+		}
+	}
+	return (str);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,11 +5,30 @@
 /**
  * *leet - This function encode a string into 1337
  *
- * @*: argument
+ * @str: argument
  *
  * Return: A value
  */
-char *leet(char *)
+char *leet(char *str)
 {
+	char *ret;
+	int i;
+	char alp_arr[] = { 'a', 'e', 'o', 't', 'l' };
+	char num_arr[] = { 4, 3, 0, 7, 1 };
 
+	ret = str;
+	i = 0;
+
+	while (*str)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*str == alp_arr[i] || *str == alp_arr[i] - 32)
+			{
+				*str = num_arr[i] + '0';
+			}
+		}
+		str++;
+	}
+	return (ret);
 }
