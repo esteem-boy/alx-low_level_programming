f40db83537a58733b7ee2eeefbf9cffcabc428e3
esteem-boy <bawaabdulsilas@gmail.com>
2023-09-13 14:44:10 -0700

    Adding task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: (0) Always
+ */
+int main(void)
+{
+	char str[9] = "_putchar";
+	int i = 0;
+
+	while (i < 9)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
